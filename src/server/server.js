14d79d990dc3494acf3c42354f94e78e9b4302cf
David de Lucas <delucas.david@gmail.com>
2020-05-07 11:21:50 +0200

    working version

--- src/server/server.js
+++ src/server/server.js
@@ -23,22 +23,41 @@ let oracles = [];
     await flightSuretyApp.methods.registerOracle().send({from: accounts[i], value: web3.utils.toWei("1", "ether"), gas: 9999999});
   }
 
+
+  /*
+  const ON_TIME = 10;
+  let events = flightSuretyApp.events.allEvents();
+  events.watch((error, result) => {
+    if (result.event === 'OracleRequest') {
+      console.log(`\n\nOracle Requested: index: ${result.args.index.toNumber()}, flight:  ${result.args.flight}, timestamp: ${result.args.timestamp.toNumber()}`);
+    } else {
+      console.log(`\n\nFlight Status Available: flight: ${result.args.flight}, timestamp: ${result.args.timestamp.toNumber()}, status: ${result.args.status.toNumber() == ON_TIME ? 'ON TIME' : 'DELAYED'}, verified: ${result.args.verified ? 'VERIFIED' : 'UNVERIFIED'}`);
+    }
+  });
+
+  await flightSuretyApp.events.allEvents({ fromBlock: 'latest' })
+  .on('data', console.log)
+  .on('changed', console.log)
+  .on('error', console.log);
+*/
+
+  // Watch contract events
   flightSuretyApp.events.OracleRequest({ fromBlock: 0 }, async function (error, event) {
     if (error) console.log(error)
     console.log(event.returnValues.index);
-    var correctIndex = event.returnValues.index;
-    var airline = event.returnValues.airline;
-    var flight = event.returnValues.flight;
-    var timestamp = event.returnValues.timestamp;
-    var flightStatusArray = [0, 10, 20, 30, 40, 50];
+    var flightStatusLst = [0, 10, 20, 30, 40, 50];
     for (var i = _orcl_ini; i < _orcl_end; i++) {
       var oracleIndexes = await flightSuretyApp.methods.getMyIndexes().call({from: accounts[i]});
       for (var j = 0; j < 3; j++) {
-        if (oracleIndexes[j] == correctIndex) {
-          var randomIndex = Math.floor(Math.random() * 6);
-          var flightStatus = flightStatusArray[randomIndex];
+        if (oracleIndexes[j] == event.returnValues.index) {
+          var flightStatus = flightStatusLst[Math.floor(Math.random() * 6)];
           console.log("flight status " + flightStatus);
-          console.log(await flightSuretyApp.methods.submitOracleResponse(oracleIndexes[j], airline, flight, timestamp, flightStatus).send({from: accounts[i]}));
+          console.log(await flightSuretyApp.methods.submitOracleResponse(
+            oracleIndexes[j], 
+            event.returnValues.airline, 
+            event.returnValues.flight, 
+            event.returnValues.timestamp, 
+            flightStatus).send({from: accounts[i]}));
         }
       }
     }
